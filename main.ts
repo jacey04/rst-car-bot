3b8f7d2108587fcb3f081213cf75587088cee43d
jacey04 <113055266+jacey04@users.noreply.github.com>
2022-10-13 14:34:12 -0400

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,12 +1,28 @@
-let distance = 0
-function sonar1 () {
-    pins.digitalWritePin(DigitalPin.P0, 0)
-    control.waitMicros(2)
-    pins.digitalWritePin(DigitalPin.P0, 1)
-    control.waitMicros(10)
-    pins.digitalWritePin(DigitalPin.P0, 0)
-    distance = pins.pulseIn(DigitalPin.P1, PulseValue.High) / 58
+input.onButtonPressed(Button.A, function () {
+    forward()
+})
+function backward () {
+    pins.servoSetPulse(AnalogPin.P8, 1700)
+    control.waitMicros(20000)
 }
+input.onButtonPressed(Button.B, function () {
+    backward()
+})
+function forward () {
+    pins.servoSetPulse(AnalogPin.P8, 1300)
+    control.waitMicros(20000)
+}
+function sensor () {
+    pins.digitalWritePin(DigitalPin.P1, 0)
+    control.waitMicros(2)
+    pins.digitalWritePin(DigitalPin.P1, 1)
+    control.waitMicros(10)
+    pins.digitalWritePin(DigitalPin.P1, 0)
+    distance = pins.pulseIn(DigitalPin.P2, PulseValue.High) / 58
+}
+let distance = 0
+basic.showIcon(IconNames.Tortoise)
+distance = 0
 basic.forever(function () {
 	
 })
